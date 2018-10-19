f058b6425eed187dbb9921e14e0869d91d3ad3ed
MilesBradt <smbradtmichael@gmail.com>
2018-10-19 08:53:19 -0700

    set basic framework for how js is going to determine final answer

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,27 @@
+var ruby = 0
+var css = 0
+var cSharp = 0
+
 $(document).ready(function() {
   $(".tracks").submit(function(event) {
-    event.preventDefault();
     var part1 = $("input:radio[name=part1]:checked").val();
-    console.log(part1);
+    if (part1 === "ruby") {
+      ruby += 1
+      }
+
+    if (part1 === "CSS") {
+        css += 1
+      }
+
+    if (part1 === "C#") {
+        cSharp += 1
+      }
+
+    console.log("Ruby " + ruby)
+    console.log("CSS " + css)
+    console.log("C# " + cSharp)
 
     $('input[type="radio"]').prop('checked', false);
+    event.preventDefault();
   });
 });
