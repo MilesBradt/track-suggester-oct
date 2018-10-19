9718a5555924620d722e067c054d03452f2ed0b7
MilesBradt <smbradtmichael@gmail.com>
2018-10-19 08:41:24 -0700

    set basic js framework to take user input from radio buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
+  $(".tracks").submit(function(event) {
+    event.preventDefault();
+    var part1 = $("input:radio[name=part1]:checked").val();
+    console.log(part1);
 
+    $('input[type="radio"]').prop('checked', false);
+  });
 });
