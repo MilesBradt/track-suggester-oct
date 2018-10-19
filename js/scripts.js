9e874b3039d9b398a2e4cb3e746d2a282929d86f
MilesBradt <smbradtmichael@gmail.com>
2018-10-19 09:08:36 -0700

    add second set of questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,45 @@
 var ruby = 0
 var css = 0
 var cSharp = 0
+var dont = 0 //it's all about the money
 
 $(document).ready(function() {
   $(".tracks").submit(function(event) {
-    var part1 = $("input:radio[name=part1]:checked").val();
-    if (part1 === "ruby") {
+    // First set
+    var data1 = $("input:radio[name=data1]:checked").val();
+    if (data1 === "Ruby") {
       ruby += 1
       }
 
-    if (part1 === "CSS") {
+    if (data1 === "CSS") {
         css += 1
       }
 
-    if (part1 === "C#") {
+    if (data1 === "C#") {
         cSharp += 1
+      }
+    // Second set
+    var data2 = $("input:radio[name=data2]:checked").val();
+    if (data2 === "Ruby") {
+      ruby += 1
+      }
+
+    if (data2 === "CSS") {
+        css += 1
+      }
+
+    if (data2 === "C#") {
+        cSharp += 1
+      }
+
+    if (data2 === "dont") {
+        dont += 1
       }
 
     console.log("Ruby " + ruby)
     console.log("CSS " + css)
     console.log("C# " + cSharp)
+    console.log("It's all about the money " + dont)
 
     $('input[type="radio"]').prop('checked', false);
     event.preventDefault();
